46e09ed6f662d330fd5d47f559144875bc3349ac
prkida <kidusasfaw97@gmail.com>
2023-10-08 22:59:30 +0300

    task2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,24 +6,8 @@
  */
 int main(void)
 {
-	int n, m;
-
-	for (n = 48; n <= 56; n++)
-	{
-		for (m = 49; m <= 57; m++)
-		{
-			if (m > n)
-			{
-				putchar(n);
-				putchar(m);
-				if (n != 56 || m != 57)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-		}
-	}
+	putchar(65);
 	putchar('\n');
-	return (0);
+	putchar(0);
+
 }
