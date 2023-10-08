572ff6cde00ea38ff9279c98c9a738a616f1bd27
prkida <kidusasfaw97@gmail.com>
2023-10-08 23:31:24 +0300

    task4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+/**
+ * main - prints all possible different combinations of three digits
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		if (n == 101 || n = 113)
+		{
+			n++;
+			continue;
+		}
+		putchar(n);
+		n++;		
+	}
+	putchar('\n');
+	return (0);
+}
