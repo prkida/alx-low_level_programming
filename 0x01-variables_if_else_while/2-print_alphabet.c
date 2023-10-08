aa8a3c4c1a87fee0349bb935a9e37abfe3e5ee85
prkida <kidusasfaw97@gmail.com>
2023-10-08 23:08:34 +0300

    print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,43 +1,19 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible different combinations of two digits
- * Return: ALways 0 (Success)
- */
-int main(void)
-{
-	#include <stdio.h>
-
-/**
  * main - prints all possible different combinations of three digits
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int n, m, l;
+	int n = 97;
 
-	for (n = 48; n < 58; n++)
+	while (n <= 122)
 	{
-		for (m = 49; m < 58; m++)
-		{
-			for (l = 50; l < 58; l++)
-			{
-				if (l > m && m > n)
-				{
-					putchar(n);
-					putchar(m);
-					putchar(l);
-					if (n != 55 || m != 56)
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-			}
-		}
+		putchar(n);
+		n++;
 	}
 	putchar('\n');
-	return (0);
-}
+	return (0);	
 
 }
